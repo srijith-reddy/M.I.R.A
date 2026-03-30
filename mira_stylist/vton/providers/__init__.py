756bb860ab9a815076dd122e7293127eab1f4e2d
Saisrijith Reddy Maramreddy <shrey.maramreddy@gmail.com>
2026-03-30 13:14:19 -0400

    Delete mira_stylist directory

--- mira_stylist/vton/providers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .base import VTONProvider
-from .remote_gpu import RemoteGPUProviderConfig, RemoteGPUVTONProvider
-
-__all__ = [
-    "RemoteGPUProviderConfig",
-    "RemoteGPUVTONProvider",
-    "VTONProvider",
-]
